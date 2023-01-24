eb88ee483c4aae5f477cc5bcf7d448133094ef5d
Daniel <d.maier020@gmail.com>
2023-01-24 13:56:59 +0100

    Initial commit

---
+++ src/settings/settings.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <array>
+#include <string>
+
+/*
+ * All settings that parametrize a simulation run.
+ */
+struct Settings
+{
+    std::array<double, 2> physicalSize;         //< physical size of the domain
+    double kappa = 4;                           //< gas constant
+    double m = 10.0;                            //< mass of an individual particle
+    double mu = 4;                              //< kinematic viscosity of the fluid
+
+    std::array<double, 2> g{0., 0.};            //< external forces
+    std::string kernelFunction = "GAUSSIAN";    //< specifies which kernel function to use
+
+
+    std::array<double, 2> dirichletBcBottom; //< prescribed values of u,v at bottom of domain
+    std::array<double, 2> dirichletBcTop;    //< prescribed values of u,v at top of domain
+    std::array<double, 2> dirichletBcLeft;   //< prescribed values of u,v at left of domain
+    std::array<double, 2> dirichletBcRight;  //< prescribed values of u,v at right of domain
+
+    //! parse a text file with settings, each line contains "<parameterName> = <value>"
+    void loadFromFile(std::string filename);
+
+    //! output all settings to console
+    void printSettings();
+};
